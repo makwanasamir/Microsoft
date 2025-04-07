f15e6eecf8c39382555dc4f7b6efced91feda41f
Samir Makwana <samir@cloudtrify.com>
2025-04-07 14:30:18 -0700

    deleted:    M365_AzureFunction_Demo/.funcignore 	deleted:    M365_AzureFunction_Demo/.gitignore 	deleted:    M365_AzureFunction_Demo/EXOMessageTrace/function.json 	deleted:    M365_AzureFunction_Demo/EXOMessageTrace/run.ps1 	deleted:    M365_AzureFunction_Demo/EXOMessageTrace/sample.dat 	deleted:    M365_AzureFunction_Demo/host.json 	deleted:    M365_AzureFunction_Demo/profile.ps1 	deleted:    M365_AzureFunction_Demo/requirements.psd1 	modified:   Scripts for Azure Functions/EnableManagedIdentity_EXO.ps1

--- Scripts for Azure Functions/EnableManagedIdentity_EXO.ps1
+++ Scripts for Azure Functions/EnableManagedIdentity_EXO.ps1
@@ -4,7 +4,7 @@ Install-Module Microsoft.Graph -Scope CurrentUser
 $tenantId = "ba8f8bff-c7fb-4629-bfb6-d6674106f7a5"
 
 #Function App Managed Identity get it from Azure Portal
-$managedIdentityIds = "d7efaefa-54d1-4bc3-8e23-821801bab9a9"#, "ANOTHER-MANAGED-ID-IF-NEEDED"
+$managedIdentityIds = "ffd12c24-c54b-478f-bd28-3a811b7e4b93"#, "ANOTHER-MANAGED-ID-IF-NEEDED"
 
 Connect-MgGraph -Scopes AppRoleAssignment.ReadWrite.All, Application.Read.All, RoleManagement.ReadWrite.Directory -TenantId $tenantId
 
